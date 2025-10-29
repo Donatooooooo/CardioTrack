105edbd1f404b8c3ce63ae736231fd70677cebf9
Fabrizio250 <fabriziorosmarino@gmail.com>
2025-10-29 10:43:19 +0100

    insert automatic Download

---
+++ predicting_outcomes_in_heart_failure/data/dataset.py
@@ -0,0 +1,24 @@
+from loguru import logger
+import typer
+import kagglehub
+import shutil
+import os
+
+
+from predicting_outcomes_in_heart_failure.config import RAW_DATA_DIR
+
+app = typer.Typer()
+
+
+@app.command()
+def main():
+    logger.info("Downloading dataset from Kaggle...")
+    os.makedirs(RAW_DATA_DIR, exist_ok=True)
+    path = kagglehub.dataset_download("fedesoriano/heart-failure-prediction")
+    shutil.copytree(path, RAW_DATA_DIR, dirs_exist_ok=True)
+    logger.success("Dataset downloaded successfully to {RAW_DATA_DIR}.")
+
+
+
+if __name__ == "__main__":
+    app()
