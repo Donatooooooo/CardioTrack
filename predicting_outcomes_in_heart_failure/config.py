0489be6046266824798e5cc13a8553c3415fe9ed
Martina Capone <mcapone977@gmail.com>
2025-11-30 19:12:27 +0100

    Modified model path

--- predicting_outcomes_in_heart_failure/config.py
+++ predicting_outcomes_in_heart_failure/config.py
@@ -117,7 +117,7 @@ FEMALE_CSV = INTERIM_DATA_DIR / "preprocessed_female_only.csv"
 PREPROCESS_ARTIFACTS_DIR = INTERIM_DATA_DIR / "preprocess_artifacts"
 SCALER_PATH = PREPROCESS_ARTIFACTS_DIR / "scaler.joblib"
 
-MODEL_PATH = Path("models/all/random_forest.joblib")
+MODEL_PATH = Path("models/nosex/random_forest.joblib")
 
 CARD_PATHS = {
     "dataset card": DATA_DIR / "README.md",
